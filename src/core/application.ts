96537c97c9f061a1e9cd8fa3a01fd86a1c59e7a9
milad145 <miladaslani1991@gmail.com>
2024-09-27 01:43:25 +0330

    adding core functions

--- src/core/application.ts
+++ src/core/application.ts
@@ -2,16 +2,68 @@ import {DatabaseService} from '../data/db';
 
 const databaseService = new DatabaseService();
 
-/**
- * create collection with custom collection name
- * @param collectionName {string}
- */
-export function createCollection (collectionName: string) {
-    return new Promise((resolve, reject) => {
-        databaseService.createCollection(collectionName)
-            .then(() => resolve(true))
-            .catch(err => reject(err))
-    })
+const application = {
+
+    /**
+     * create collection
+     * @param collectionName {string}
+     */
+    async createCollection(collectionName: string) {
+        try {
+            await databaseService.createCollection(collectionName)
+            return true;
+        } catch (e) {
+            throw e
+        }
+    },
+
+    /**
+     * delete specific collection by name
+     * @param collectionName {string}
+     */
+    async deleteCollection(collectionName: string) {
+        try {
+            await databaseService.deleteCollection(collectionName)
+            return true
+        } catch (e) {
+            throw e
+        }
+    },
+
+    async addRecord(collectionName: string, record: { foo: string, bar: string, baz: number }) {
+        try {
+            await databaseService.addRecord(collectionName, record)
+            return true
+        } catch (e) {
+            throw e
+        }
+    },
+
+    async getRecords(tableName: string, limit: number, skip: number) {
+        try {
+            return await databaseService.getRecords(tableName, limit, skip)
+        } catch (e) {
+            throw e
+        }
+    },
+
+    async deleteRecord(tableName: string, baz: number) {
+        try {
+            await databaseService.deleteRecord(tableName, baz)
+            return true
+        } catch (e) {
+            throw e
+        }
+    },
+
+    async updateRecord(tableName: string, baz: number, record: { foo?: string, bar?: string }) {
+        try {
+            await databaseService.updateRecord(tableName, baz, record)
+            return true
+        } catch (e) {
+            throw e
+        }
+    }
 }
 
-
+export default application
